3000064f7712f73aa2fa112590adc3b992e0ed4a
unknown <1011233891@qq.com>
2019-12-23 11:28:11 +0800

    旅游

---
+++ src/pages/home/components/lcons.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="icons">
+        <swiper :options="swiperOption">
+                <swiper-slide v-for='(page, index) of page' :key='index'>
+                    <div class="icon" v-for="item of page" :key="item.id">
+                            <div class="icon-img">
+                                <img class="con-img-content" :src="item.imgUrl">
+                            </div>
+                            <p class="icon">
+                    </div>
+                </swiper-slide>
+        </swiper>
+    </div>
+</template>
